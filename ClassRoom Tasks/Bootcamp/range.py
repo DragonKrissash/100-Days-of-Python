378f827785ca3dcae2dc7aa82e4962498c47ae65
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-04-17 12:22:04 +0530

    16-4-2024

---
+++ ClassRoom Tasks/Bootcamp/range.py
@@ -0,0 +1,9 @@
+# def odd_num():
+#     n=int(input("Enter the n: "))
+#     print(list(range(1,n,2)))
+# odd_num()
+
+def first_n_odd():
+    n=int(input("Enter number of terms: "))
+s=list(map(int,input().split()))
+print(s)
