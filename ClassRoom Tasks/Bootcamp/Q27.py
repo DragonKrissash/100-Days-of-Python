68907c27e260666592a61c0c0eb7078d2203d9cb
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-04-17 10:45:43 +0530

    17-4-2024

---
+++ ClassRoom Tasks/Bootcamp/Q27.py
@@ -0,0 +1,14 @@
+#Grade based on marks
+marks=int(input('Enter the marks of a student: '))
+if marks>=90:
+    print('A+')
+elif marks>=80:
+    print('A')
+elif marks>=70:
+    print('B')
+elif marks>=60:
+    print('C')
+elif marks>=50:
+    print('D')
+else:
+    print('F')
