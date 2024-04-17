36f18c7292f34d52cdd9439dbabf9779b4304ae1
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-04-17 10:30:28 +0530

    17-4-2024

---
+++ ClassRoom Tasks/Bootcamp/Q21.py
@@ -0,0 +1,5 @@
+#Celsius to fahrenheit and kelvin
+cel=int(input("Enter the temperature in celsius: "))
+fah=(9/5)*cel+32
+kel=273+cel
+print(f'The temperature in fahrenheit: {fah}\nThe temperature in kelvin: {kel}')
