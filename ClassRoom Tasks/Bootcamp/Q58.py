8f1206fe0fc83aa9dbd3e462154c4e4dbe16d9ad
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-04-17 17:39:52 +0530

    16-4-2024

---
+++ ClassRoom Tasks/Bootcamp/Q58.py
@@ -0,0 +1,13 @@
+row1=int(input('Enter the number of rows of matrix 1: '))
+col1=int(input('Enter the number of cols of matrix 1: '))
+mat1=[]
+print("Enter the elements of matrix 1: ")
+for i in range(row1):
+    mat1.append(list(map(int,input())))
+row2=int(input('Enter the number of rows of matrix 2: '))
+col2=int(input('Enter the number of cols of matrix 2: '))
+mat2=[]
+print("Enter the elements of matrix 2: ")
+for i in range(row2):
+    mat2.append(list(map(int,input())))
+
