45f6076443f380be08c4f09745863663a19305ce
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-04-18 11:43:02 +0530

    16-4-2024

--- ClassRoom Tasks/Bootcamp/Q57.py
+++ ClassRoom Tasks/Bootcamp/Q57.py
@@ -1,3 +1,16 @@
-l=list(map(int,input('Enter the array elements: ').split()))
-n=int(input('Enter the circular index: '))
-print(f'{l[n:]}{l[:n]}')
+# l=list(map(int,input('Enter the array elements: ').split()))
+# n=int(input('Enter the circular index: '))
+# print(f'{l[n:]}{l[:n]}')
+# import math
+#
+# n=int(input('Enter a number: '))
+# x=int(math.log(n-n%2,2))
+# print(x)
+
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=a
+for i in b:
+    if i not in a:
+        c.append(i)
+print(c)
